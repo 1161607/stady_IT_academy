03688a6cc8d0e765ea9e09bd5b0e3cd65a2a725a
1161607 <1161607@gmail.com>
2022-04-23 16:27:56 +0300

    added calculator code

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,23 @@
 print('Hello, world!')
+
+# simple calculator
+
+a = float(input('Введите число '))
+b = float(input('Введите число '))
+
+what = input('Выберите действие: +, -, /, * ')
+
+if what == '+':
+    c = a + b
+    print('Результат = ', c)
+elif what == '-':
+    c = a - b
+    print('Результат = ', c)
+elif what == '/':
+    c = a/b
+    print('Результат = ', c)
+elif what == '*':
+    c = a * b
+    print('Результат = ', c)
+else:
+    print('выбранное неверное действие')
