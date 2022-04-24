0f71f199ef0afc5d750246c9ee92003792845a08
1161607 <1161607@gmail.com>
2022-04-24 15:14:05 +0300

    woking sunday (((

--- hello_world.py
+++ hello_world.py
@@ -1,27 +1,5 @@
 print('Hello, world!')
 
-# simple calculator
+print('Today is saturday but i learn github((')
 
-a = float(input('Введите число '))
-b = float(input('Введите число '))
-
-what = input('Выберите действие: +, -, /, * ')
-
-if what == '+':
-    c = a + b
-    print('Результат = ', c)
-elif what == '-':
-    c = a - b
-    print('Результат = ', c)
-elif what == '/':
-    c = a/b
-    print('Результат = ', c)
-elif what == '*':
-    c = a * b
-    print('Результат = ', c)
-else:
-    print('выбранное неверное действие')
-    
-    # new add
-    
-    print('Today is saturday but i learn github((')
+print('now allready sunday but i learn github again')
